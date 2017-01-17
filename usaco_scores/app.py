656f1f64103d4216897f0479896480898ef623d0
Kevin Geng <gengkev@gmail.com>
2017-01-17 01:18:55 -0500

    show only us scores by default

--- usaco_scores/app.py
+++ usaco_scores/app.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
-from flask import Blueprint, render_template
+from flask import Blueprint, request, render_template
 from .models import *
 
 page = Blueprint('usaco_scores', __name__)
 
 @page.route('/')
 def home():
+    international = request.args.get('international', type=bool)
     query = Competitor.select(Competitor,
         SQL('score_dec + score_jan + score_feb').alias('score_total'))
+    if not international:
+        query = query.where(Competitor.country == 'USA')
     query = query.order_by(SQL('score_total').desc())
     return render_template('scores.html', query=query)
 
