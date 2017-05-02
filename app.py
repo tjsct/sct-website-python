a90afea00e62833de77d581ce495d45aabaa9fab
Kevin Geng <gengkev@gmail.com>
2017-05-02 00:36:22 -0400

    Write About page, replacing Competitions
    im just procrastinating probably

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 import os
 
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory, redirect, url_for
 
 app = Flask(__name__)
 
@@ -15,45 +15,47 @@ import util
 lectures_by_year = util.get_lectures_by_year()
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/index.php', methods=['GET'])
-def home2():
-    return render_template('index.html')
+@app.route('/about.php')
+def about():
+    return render_template('about.html')
 
-@app.route('/competitions.php', methods=['GET'])
-def competitions():
-    return render_template('competitions.html')
-
-@app.route('/lectures.php', methods=['GET'])
+@app.route('/lectures.php')
 def lectures():
     return render_template('lectures.html',
             lectures_by_year=lectures_by_year)
 
-@app.route('/schedule.php', methods=['GET'])
+@app.route('/schedule.php')
 def schedule():
     return render_template('schedule.html')
 
-@app.route('/links.php', methods=['GET'])
+@app.route('/links.php')
 def links():
     return render_template('links.html')
 
-@app.route('/styles.css', methods=['GET'])
+@app.route('/styles.css')
 def styles():
     return app.send_static_file('styles.css')
 
 
+# redirects
+@app.route('/index.php')
+def home2():
+    return redirect(url_for('home'))
+
+@app.route('/competitions.php')
+def competitions():
+    return redirect(url_for('about'))
+
+
+# serve lecture files
+
 @app.route('/lectures/<path:path>')
 def lecture_files(path):
     return send_from_directory('lectures', path)
-
-
-@app.after_request
-def after_request(response):
-    response.headers['Server'] = 'nginx/1.8.1'
-    return response
 
 
 if __name__ == '__main__':
